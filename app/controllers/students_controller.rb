79da816c92ba4402e6e75de76f4f7280cc1ddffc
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-03-15 22:15:05 +0300

    commit tree f59d1f336518881237fb046d35f845d4c755b7c4

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # students = Student.all
+    # render json: students
+    if params[:name]
+      # If the "name" parameter is present, find all students whose first or last name matches
+      @students = Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{params[:name]}%", "%#{params[:name]}%")
+    else
+      # If no parameters are present, return all students
+      @students = Student.all
+    end
+    render json: @students
+  end
+
+  def show
+    single_student= Student.find(params[:id])
+    render json: single_student
   end
 
 end
